52ce22bfe0b4cd1e740449fc7f022f335b0301f1
MaheswariSannepalli <100559527+MaheswariSannepalli@users.noreply.github.com>
2022-08-11 19:08:42 +0530

    Add files via upload

---
+++ Backend/WebsiteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteProjectApplication.class, args);
+	}
+
+}
